5841d129d5795678e605f2721103abf5ec2f6447
DiemTrinh1513636 <diemtrinhvlm97@gmail.com>
2018-12-05 20:42:31 +0700

    admin

---
+++ admin/addUser.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - Dark Admin</title>
+
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="asset/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="asset/css/local.css" />
+
+    <script type="text/javascript" src="asset/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="asset/js/bootstrap.min.js"></script>   
+
+    <style>
+        div {
+            padding-bottom:20px;
+        }
+    </style>
+</head>
+<body>
+    <div id="wrapper">
+        <?php
+            include("common.php");
+        ?>
+        <div id="add-user">
+            <div class="row text-center">
+                <h2>Thêm User</h2>
+            </div>
+            <div>
+                <label for="firstname" class="col-md-2">
+                    Tên đăng nhập:
+                </label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" id="firstname" >
+                </div>
+            </div>        
+            <div>
+                <label for="password" class="col-md-2">
+                    Mật khẩu:
+                </label>
+                <div class="col-md-9">
+                    <input type="password" class="form-control" id="password" >
+                </div>
+                
+            </div>
+            <div>
+                <label for="re-password" class="col-md-2">
+                    Nhập lại mật khẩu:
+                </label>
+                <div class="col-md-9">
+                    <input type="password" class="form-control" id="re-password" >
+                </div>
+            </div>
+            <div>
+                <label for="email" class="col-md-2">
+                    Email address:
+                </label>
+                <div class="col-md-9">
+                    <input type="email" class="form-control" id="email" >
+                    <p class="help-block">
+                        Ví dụ: yourname@domain.com
+                    </p>
+                </div>
+            </div>
+            <div>
+                <label for="fullname" class="col-md-2">
+                    Họ và tên: 
+                </label>
+                <div class="col-md-9">
+                    <input type="text" class="form-control" id="fullname" >
+                </div>
+            </div>
+            <div>
+                <label for="birthday" class="col-md-2">Ngày sinh:</label>
+                <div class="col-md-9">
+                    <input type="date" value="" id="birthday" name="birthday">
+                </div>
+            </div>
+            <div>
+                <label for="sex" class="col-md-2">
+                    Giới tính:
+                </label>
+                <div class="col-md-10">
+                    <label class="radio">
+                        <input type="radio" name="sex" id="sex" value="male" checked>
+                        Male
+                    </label>
+                    <label class="radio">
+                        <input type="radio" name="sex" id="sex" value="female">
+                        Female
+                    </label>
+                    <label class="radio">
+                        <input type="radio" name="sex" id="sex" value="female">
+                        Other
+                    </label>
+                </div>             
+            </div>
+            <div>
+                <label for="captcha" class="col-md-2">Mã xác nhận:</label>
+                <div class="col-md-6">
+                    <input class="form-control" type="text" value="" id="captcha" name="captcha">
+                </div>
+                <div class="col-md-3">
+                    <img src="asset/image/captcha/captcha1.jpg" alt="captcha" width="30%" height=30%>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9"></div>
+                <div class="col-md-3">
+                    <input class="btn btn-primary" type="submit" value="Đăng ký">
+                </div>
+            </div>
+            
+        </div>  
+    </div>
+
+</body>
+</html>
---
+++ admin/common.php
@@ -0,0 +1,78 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+			<span class="sr-only">Toggle navigation</span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="index.html">Admin Panel</a>
+	</div>
+	<div class="collapse navbar-collapse navbar-ex1-collapse">
+		<ul id="active" class="nav navbar-nav side-nav">
+			<li>
+				<a href="index.php">
+					<i class="fa fa-bullseye"></i> Dashboard</a>
+			</li>
+			<li>
+				<a href="post.php">
+					<i class="fa fa-tasks"></i> Post film</a>
+			</li>
+			<li>
+				<a href="addUser.php">
+					<i class="fa fa-user-plus"></i> Add user</a>
+			</li>
+			<li>
+				<a href="deleteUser.php">
+					<i class="fa fa-edit"></i> Delete and Edit User</a>
+			</li>
+		</ul>
+		<ul class="nav navbar-nav navbar-right navbar-user">
+			<!-- <li class="dropdown messages-dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> Messages <span class="badge">2</span> <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-header">2 New Messages</li>
+                <li class="message-preview">
+                    <a href="#">
+                        <span class="avatar"><i class="fa fa-bell"></i></span>
+                        <span class="message">Security alert</span>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li class="message-preview">
+                    <a href="#">
+                        <span class="avatar"><i class="fa fa-bell"></i></span>
+                        <span class="message">Security alert</span>
+                    </a>
+                </li>
+                <li class="divider"></li>
+                <li><a href="#">Go to Inbox <span class="badge">2</span></a></li>
+            </ul>
+        </li> -->
+			<li>
+				<form class="navbar-search">
+					<input type="text" placeholder="Search" class="form-control">
+				</form>
+			</li>
+			<li class="divider-vertical"></li>
+			<li class="dropdown user-dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+					<i class="fa fa-user"></i> Steve Miller
+					<b class="caret"></b>
+				</a>
+				<ul class="dropdown-menu">
+					<li>
+						<a href="#">
+							<i class="fa fa-user"></i> Profile</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fa fa-power-off"></i> Log Out</a>
+					</li>
+
+				</ul>
+			</li>
+
+		</ul>
+	</div>
+</nav>
