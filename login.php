23bd6aff782956b2e8d552e4ef7511f4035a33f0
Nguyen Thi Bich Van <ngthibichvancs@gmail.com>
2018-12-06 13:00:51 +0700

    Van

---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+    session_start();
+?>
+
+<?php
+    require_once("libs/db.php");
+    if(isset($_POST["btn_login"])){
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+            
+        $username = strip_tags($username);
+		$username = addslashes($username);
+		$password = strip_tags($password);
+		$password = addslashes($password);
+		if ($username == "" || $password =="") {
+			echo "username và password bạn không được để trống!";
+        }
+        else{
+			$sql = "SELECT * FROM user WHERE username = '$username'";
+            $result = mysqli_query($link,$sql);
+            if(!$result || (mysqli_num_rows($result) < 1)){
+                echo "Username không đúng";
+            }
+            $dbarray = mysqli_fetch_array($result); 
+            if(password_verify($password,$dbarray["password"])){
+                $_SESSION['username'] = $username;
+                $_SESSION['password'] = $password;
+                echo "Success!";
+                // phân quyền
+                if($dbarray['usertype'] == 99){
+                    header('Location:admin/index.php');
+                }
+                else{
+                    //member
+                    header('Location:index.php');
+                    //guest ??
+                    header('Location:index.php');
+                }
+            }
+            else{
+                echo " Fail";
+                // echo "<script>
+                //     alert('Password failure');
+                // </script>";
+            }
+		}
+    }
+?>
