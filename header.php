c2e5a41d959d6f9bc0a11e2bbf817dda7bf1b30b
Nguyen Thi Bich Van <ngthibichvancs@gmail.com>
2018-12-06 13:15:15 +0700

    hhh

--- header.php
+++ header.php
@@ -38,14 +38,6 @@
     }
   });
 </script>
-<?php
-  if(isset($_SESSION['username']) && isset($_SESSION['password'])){
-    <script>
-      echo haha;
-      .login 
-    </script>
-  }
-?>
 <!-- Van modified ↑↑ -->
 <div id="nav">
   <ul class="container menu">
